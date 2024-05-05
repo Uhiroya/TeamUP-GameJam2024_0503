b89c42235cd1d4b474319b4394b714f05ff18da5
hiropu0824 <hiropu0824@gmail.com>
2024-05-05 23:45:57 +0900

    栄養剤排出口の移動制限

--- Assets/MyGame/Scripts/InGame/WaterDrop/DropPortManager.cs
+++ Assets/MyGame/Scripts/InGame/WaterDrop/DropPortManager.cs
@@ -31,7 +31,8 @@ public class DropPortManager : SingletonMonoBehavior<DropPortManager>
     [SerializeField, Header("発射口")] private Transform _dropPort;
     [SerializeField　,Header("垂らす液体")] private GameObject _dropPrefab;
     [SerializeField ,Header("移動スピード")] private float _moveSpeed;
-    [SerializeField, Header("移動制限")] private Vector3 _limit;
+    [SerializeField, Header("移動制限プラス方向")] private Vector3 _limitPlusVector;
+    [SerializeField, Header("移動制限マイナス方向")] private Vector3 _limitMinusVector;
     
     [SerializeField , Header("デフォルトのステータス")] public DropPortManagerStatus _defaultStatus;
     
@@ -74,8 +75,8 @@ public class DropPortManager : SingletonMonoBehavior<DropPortManager>
         var portPos = _launchPort.position;
         var railPos = _launchRail.position;
         
-        var newPosX = Mathf.Clamp( portPos.x + input.x * _moveSpeed, -_limit.x, _limit.x);
-        var newPosZ = Mathf.Clamp( railPos.z + input.z * _moveSpeed, -_limit.z, _limit.z);
+        var newPosX = Mathf.Clamp( portPos.x + input.x * _moveSpeed, _limitMinusVector.x, _limitPlusVector.x);
+        var newPosZ = Mathf.Clamp( railPos.z + input.z * _moveSpeed, _limitMinusVector.z, _limitPlusVector.z);
         
         _launchPort.position = new Vector3(newPosX , portPos.y , portPos.z);
         _launchRail.position = new Vector3(railPos.x , railPos.y , newPosZ);
