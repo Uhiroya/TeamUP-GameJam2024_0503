1f01d324d06ff031f1e183da40ea43b0012fe156
hiropu0824 <hiropu0824@gmail.com>
2024-05-05 20:52:41 +0900

    Camera機能の調整

--- Assets/MyGame/Scripts/InGame/Camera/CameraRotator.cs
+++ Assets/MyGame/Scripts/InGame/Camera/CameraRotator.cs
@@ -1,64 +1,58 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class CameraRotator : MonoBehaviour
+public class CameraRotator : MonoBehaviour , IDragHandler
 {
-    public GameObject target;
-    public Vector2 rotationSpeed;
-    public bool reverse;
+    public Vector2 RotationSpeed;
 
     [SerializeField]  private float _threshold;
-    private Vector2 lastMousePosition;
-    private Vector2 newAngle = new Vector2(0, 0);
+    private Vector2 _lastMousePosition;
+    private Vector2 _newAngle = new Vector2(0, 0);
 
-    // ゲーム実行中の繰り返し処理
+    private Quaternion _defaultRotation;
+    [SerializeField] float _limitRotationX = 80f;
+    [SerializeField] float _limitRotationY = 80f;
+    
+    bool _isDrag;
+    private void Start()
+    {
+        _defaultRotation = transform.rotation;
+    }
+    
     void Update()
     {
+        if (!_isDrag && EventSystem.current.IsPointerOverGameObject()) return;
+
         // 左クリックした時
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButtonDown(0))
         {
-            newAngle = target.transform.localEulerAngles;
-            lastMousePosition = Input.mousePosition;
+            _isDrag = true;
+            _lastMousePosition = Input.mousePosition;
         }
         // 左ドラッグしている間
-        else if (Input.GetMouseButton(1))
+        if (Input.GetMouseButton(0) && _isDrag)
         {
-            if (((Vector2)Input.mousePosition - lastMousePosition).magnitude < _threshold) return;
-            if (!reverse)
-            {
-                
-                newAngle.x -= (Input.mousePosition.y - lastMousePosition.y) * rotationSpeed.x;
-                newAngle.y -= (lastMousePosition.x - Input.mousePosition.x) * rotationSpeed.y;
+            if (((Vector2)Input.mousePosition - _lastMousePosition).magnitude < _threshold) return;
+            _newAngle.x -= (Input.mousePosition.y - _lastMousePosition.y) * RotationSpeed.x;
+            _newAngle.y -= (_lastMousePosition.x - Input.mousePosition.x) * RotationSpeed.y;
+            
+            _newAngle.x = Mathf.Clamp(_newAngle.x, -_limitRotationX,   _limitRotationX);
+            _newAngle.y = Mathf.Clamp(_newAngle.y,  -_limitRotationY,  _limitRotationY);
 
-                
-                target.transform.localEulerAngles = newAngle;
-                lastMousePosition = Input.mousePosition;
-            }
-            else if (reverse)
-            {
-                newAngle.y -= (Input.mousePosition.x - lastMousePosition.x) * rotationSpeed.y;
-                newAngle.x -= (lastMousePosition.y - Input.mousePosition.y) * rotationSpeed.x;
-                target.transform.localEulerAngles = newAngle;
-                lastMousePosition = Input.mousePosition;
-            }
+            transform.rotation = _defaultRotation * Quaternion.Euler(_newAngle.x , _newAngle.y , 0f);
+            _lastMousePosition = Input.mousePosition;
+        }
+        else
+        {
+            _isDrag = false;
         }
     }
 
-    // マウスドラッグ方向と視点回転方向を反転する処理
-    public void DirectionChange()
+    public void OnDrag(PointerEventData eventData)
     {
-        // 判定フラグ変数"reverse"が"false"であれば
-        if (!reverse)
-        {
-            // 判定フラグ変数"reverse"に"true"を代入
-            reverse = true;
-        }
-        // でなければ（判定フラグ変数"reverse"が"true"であれば）
-        else
-        {
-            // 判定フラグ変数"reverse"に"false"を代入
-            reverse = false;
-        }
+        Debug.Log("ドラッグ中");
     }
 }
